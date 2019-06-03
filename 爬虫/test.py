8e76719b437e3bc58d3fb6d3ae2d44e5faaa0b59
chengmingzhang <18216642724@163.com>
2019-06-03 16:24:52 +0800

    排行榜单小说爬取，保存在本地

--- 爬虫/test.py
+++ 爬虫/test.py
@@ -5,6 +5,7 @@
 import requests
 from pyquery import PyQuery as pq
 
+# 进入排行榜，获取排行榜的小说链接，总、月、周、日榜的小说链接地址去重，得到difference_link
 url = 'http://www.xbiquge.la/paihangbang/'
 response = requests.get(url)
 response.encoding = response.apparent_encoding
@@ -14,23 +15,25 @@ difference_link = set()
 for link in links.items():
     difference_link.add(link.attr.href)
 
+# 得到了排行榜的小说，逐个获取小说的内容
 for link in difference_link:
-    response1 = requests.get(link)
-    response1.encoding = response1.apparent_encoding
-    doc1 = pq(response1.text)
-    links1 = doc1('#list > dl >dd >a')
+    response = requests.get(link)
+    response.encoding = response.apparent_encoding
+    doc = pq(response.text)
+    links = doc('#list > dl >dd >a')  # 排行榜对应小说页面，获取所有的章节链接
+    name = doc('#info > h1').text()
+    with open(file=name + '.txt', mode='a+', encoding='utf-8') as f:
 
-    for link1 in links1.items():
-        url1 = 'http://www.xbiquge.la/' + link1.attr.href
-        response1 = requests.get(link)
-        response1.encoding = response1.apparent_encoding
-        doc1 = pq(response1.text)
-        title = doc1('#wrapper > div.content_read > div > div.bookname > h1').text()
-        content = doc1('#content').text()
-        name = doc1('#info > h1').text()
-        print(name, url1, title)
+        # 获取所有的章节链接的内容
+        for link1 in links.items():
+            url = 'http://www.xbiquge.la/' + link1.attr.href
+            response = requests.get(url)
+            response.encoding = response.apparent_encoding
+            doc1 = pq(response.text)
+            title = doc1('#wrapper > div.content_read > div > div.bookname > h1').text()
+            content = doc1('#content').text()
+            print('正在爬取 %s %s' % (name, title))
 
-        with open(file=name+'.txt', mode='a+', encoding='utf-8') as f:
             f.write(title)
             f.write('\n')
             f.write(content)
